057ed795a1001d0608e7af1acd94c370923cae1b
gudrms <gudrmsglgl@gmail.com>
2019-05-15 12:20:23 +0900

    update test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    // update test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
