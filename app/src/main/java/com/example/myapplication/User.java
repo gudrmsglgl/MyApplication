7db50d05cb75881ce005be14f388af727f510027
gudrms <gudrmsglgl@gmail.com>
2019-05-15 12:12:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/User.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+public class User {
+    public final String firstName;
+    public final String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+
+}
